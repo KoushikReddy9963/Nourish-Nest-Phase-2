e9dd8c6682df7273516af0ee94dd1fcc95ba5116
Y Sai Koushik Reddy <152732336+KoushikReddy9963@users.noreply.github.com>
2025-02-20 21:13:08 +0530

    Updated views.js

--- src/Controller/index.js
+++ src/Controller/index.js
@@ -17,7 +17,7 @@ console.log(__dirname);
 const app = express();
 
 app.set("view engine", "ejs");
-app.set("views", "./views");
+app.set("views", "../../views");
 
 app.use(express.static(path.join(__dirname, "../../public")));
 app.use(express.urlencoded({ extended: true }));
